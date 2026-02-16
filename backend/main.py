d4f96b5743689cc3cbb59110daa2d2b09b41fe5e
Ahmad Khidir <khidirahmad05@gmail.com>
2026-02-17 00:46:42 +0100

    feat: Update onl FastAPI backend with serverless deployment using AWS SAM and Mangum.

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from src.api.v1 import router as v1_router
 from mangum import Mangum
+from aws_lambda_powertools.logging import Logger
 
+logger = Logger()
 
 app = FastAPI()
 
 app.include_router(router=v1_router, prefix="/v1")
 
+
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def read_root(request: Request):
+    logger.info(f"Hello: {request.scope.get('aws.event')}")
+    return {"Hello": request.scope.get("aws.event")}
 
 
 handler = Mangum(app)
